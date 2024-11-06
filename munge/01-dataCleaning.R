de69253be988aae92c89391de0c47fa1a5a59b26
your-username <varshavenkat2001@gmail.com>
2024-11-06 22:43:55 +0000

    Created a folder inside the cache and moved the pre processed data and final data inside it and made the data folder to have only the raw data files. Now the pre processed data and the data frames are in the cache folder which can be used in the rmd file

---
+++ munge/01-dataCleaning.R
@@ -0,0 +1,85 @@
+file_paths <- c(
+    "~/Desktop/courseWorkCS8631/data/cyber-security-1_enrolments.csv",
+    "~/Desktop/courseWorkCS8631/data/cyber-security-2_enrolments.csv",
+    "~/Desktop/courseWorkCS8631/data/cyber-security-3_enrolments.csv",
+    "~/Desktop/courseWorkCS8631/data/cyber-security-4_enrolments.csv",
+    "~/Desktop/courseWorkCS8631/data/cyber-security-5_enrolments.csv",
+    "~/Desktop/courseWorkCS8631/data/cyber-security-6_enrolments.csv",
+    "~/Desktop/courseWorkCS8631/data/cyber-security-7_enrolments.csv"
+)
+cleaned_file_paths <- c(
+    "~/Desktop/courseWorkCS8631/cache/preData/cleaned_cyber_security_1.csv",
+    "~/Desktop/courseWorkCS8631/cache/preData/cleaned_cyber_security_2.csv",
+    "~/Desktop/courseWorkCS8631/cache/preData/cleaned_cyber_security_3.csv",
+    "~/Desktop/courseWorkCS8631/cache/preData/cleaned_cyber_security_4.csv",
+    "~/Desktop/courseWorkCS8631/cache/preData/cleaned_cyber_security_5.csv",
+    "~/Desktop/courseWorkCS8631/cache/preData/cleaned_cyber_security_6.csv",
+    "~/Desktop/courseWorkCS8631/cache/preData/cleaned_cyber_security_7.csv"
+)
+output_dir <- "~/Desktop/courseWorkCS8631/cache/preData"
+if (!dir.exists(output_dir)) {
+    dir.create(output_dir, recursive = TRUE)
+}
+
+for (i in seq_along(file_paths)) {
+    cleaned_data <- read.csv(file_paths[i]) %>%
+        select(learner_id, highest_education_level, employment_area) %>%
+        filter(!is.na(learner_id))
+    write.csv(cleaned_data, cleaned_file_paths[i], row.names = FALSE)
+}
+
+
+file_paths_leaving_survey_responses <- c(
+    "~/Desktop/courseWorkCS8631/data/cyber-security-1_leaving-survey-responses.csv",
+    "~/Desktop/courseWorkCS8631/data/cyber-security-2_leaving-survey-responses.csv",
+    "~/Desktop/courseWorkCS8631/data/cyber-security-3_leaving-survey-responses.csv",
+    "~/Desktop/courseWorkCS8631/data/cyber-security-4_leaving-survey-responses.csv",
+    "~/Desktop/courseWorkCS8631/data/cyber-security-5_leaving-survey-responses.csv",
+    "~/Desktop/courseWorkCS8631/data/cyber-security-6_leaving-survey-responses.csv",
+    "~/Desktop/courseWorkCS8631/data/cyber-security-7_leaving-survey-responses.csv"
+)
+
+cleaned_file_paths_leaving_survey_responses <- c(
+    "~/Desktop/courseWorkCS8631/cache/preData/cleaned_cyber_security_1_leaving_survey_responses.csv",
+    "~/Desktop/courseWorkCS8631/cache/preData/cleaned_cyber_security_2_leaving_survey_responses.csv",
+    "~/Desktop/courseWorkCS8631/cache/preData/cleaned_cyber_security_3_leaving_survey_responses.csv",
+    "~/Desktop/courseWorkCS8631/cache/preData/cleaned_cyber_security_4_leaving_survey_responses.csv",
+    "~/Desktop/courseWorkCS8631/cache/preData/cleaned_cyber_security_5_leaving_survey_responses.csv",
+    "~/Desktop/courseWorkCS8631/cache/preData/cleaned_cyber_security_6_leaving_survey_responses.csv",
+    "~/Desktop/courseWorkCS8631/cache/preData/cleaned_cyber_security_7_leaving_survey_responses.csv"
+)
+output_dir_leaving_survey_responses <- "~/Desktop/courseWorkCS8631/cache/preData"
+if (!dir.exists(output_dir_leaving_survey_responses)) {
+    dir.create(output_dir_leaving_survey_responses, recursive = TRUE)
+}
+for (i in seq_along(file_paths_leaving_survey_responses)) {
+    cleaned_data <- suppressWarnings(read.csv(file_paths_leaving_survey_responses[i])) %>%
+        select(learner_id, left_at, leaving_reason) %>%
+        filter(!is.na(learner_id))
+    write.csv(cleaned_data, cleaned_file_paths_leaving_survey_responses[i], row.names = FALSE)
+}
+
+
+
+required_columns <- c("learner_id", "enrolled_at", "unenrolled_at", "highest_education_level", "employment_area")
+# Function to ensure each dataframe has all required columns
+ensure_columns <- function(df, required_cols) {
+    missing_cols <- setdiff(required_cols, names(df))
+    df[missing_cols] <- NA  # Add missing columns with NA values
+    return(df)
+}
+# Apply this function to each dataframe in the list
+cleaned_data_list <- lapply(file_paths, read.csv)
+cleaned_data_list <- lapply(cleaned_data_list, ensure_columns, required_cols = required_columns)
+combined_cleaned_data <- bind_rows(cleaned_data_list)
+# Now proceed with your summarization
+duplicate_counts <- combined_cleaned_data %>%
+    group_by(learner_id) %>%
+    summarise(
+        count = n_distinct(enrolled_at, unenrolled_at, highest_education_level, employment_area),
+        unique_enrolled_at = unique(enrolled_at) %>% paste(collapse = ", "),
+        unique_unenrolled_at = unique(unenrolled_at) %>% paste(collapse = ", ")
+    ) %>%
+    filter(count > 1) %>%
+    ungroup()
+write.csv(duplicate_counts, "~/Desktop/courseWorkCS8631/cache/preData/duplicate_counts.csv", row.names = FALSE)
---
+++ munge/03-dataTransformation.R
@@ -0,0 +1,56 @@
+combined_data <- read.csv("~/Desktop/courseWorkCS8631/cache/finalData/combined_cleaned_cyber_security.csv")
+combined_data <- combined_data %>%
+    mutate(
+        highest_education_level = ifelse(highest_education_level == "Unknown", NA, highest_education_level),
+        employment_area = ifelse(employment_area == "Unknown", NA, employment_area)
+    )
+combined_data <- combined_data %>%
+    mutate(employment_area = recode(employment_area,
+                                    "accountancy_banking_and_finance" = "Finance",
+                                    "armed_forces_and_emergency_services" = "Defense",
+                                    "business_consulting_and_management" = "Consulting",
+                                    "charities_and_voluntary_work" = "Charity",
+                                    "creative_arts_and_culture" = "Arts",
+                                    "energy_and_utilities" = "Energy",
+                                    "engineering_and_manufacturing" = "Engineering",
+                                    "environment_and_agriculture" = "Environment",
+                                    "health_and_social_care" = "Healthcare",
+                                    "hospitality_tourism_and_sport" = "Hospitality",
+                                    "it_and_information_services" = "IT",
+                                    "law" = "Law",
+                                    "marketing_advertising_and_pr" = "Marketing",
+                                    "media_and_publishing" = "Media",
+                                    "property_and_construction" = "Construction",
+                                    "public_sector" = "Public",
+                                    "recruitment_and_pr" = "Recruitment",
+                                    "retail_and_sales" = "Retail",
+                                    "science_and_pharmaceuticals" = "Science",
+                                    "teaching_and_education" = "Education",
+                                    "transport_and_logistics" = "Logistics"
+    ))
+write.csv(combined_data, "~/Desktop/courseWorkCS8631/cache/finalData/combined_cleaned_cyber_security.csv", row.names = FALSE)
+
+duplicates_data <- read.csv("~/Desktop/courseWorkCS8631/cache/preData/duplicate_counts.csv")
+duplicates_data <- duplicates_data %>%
+    separate(unique_enrolled_at, into = paste0("enrolled_at_", 1:7), sep = ", ", fill = "right") %>%
+    mutate(across(starts_with("enrolled_at_"), ~ as.Date(sub(" UTC", "", .), format="%Y-%m-%d")))
+duplicates_data <- duplicates_data %>%
+    separate(unique_unenrolled_at, into = paste0("unenrolled_at_", 1:7), sep = ", ", fill = "right") %>%
+    mutate(across(starts_with("unenrolled_at_"), ~ as.Date(sub(" UTC", "", .), format="%Y-%m-%d")))
+write.csv(duplicates_data, "~/Desktop/courseWorkCS8631/cache/finalData/duplicate_counts_separated_dates.csv", row.names = FALSE)
+
+duplicates_data_leaving_survey_responses <- read.csv("~/Desktop/courseWorkCS8631/cache/finalData/combined_cleaned_cyber_security_leaving_survey_responses.csv",fileEncoding = "UTF-8")
+duplicates_data_leaving_survey_responses <- duplicates_data_leaving_survey_responses %>%
+    mutate(left_at = format(as.Date(sub(" .*", "", left_at), format="%Y-%m-%d"), "%d-%m-%Y"))
+write.csv(duplicates_data_leaving_survey_responses, "~/Desktop/courseWorkCS8631/cache/finalData/combined_cleaned_cyber_security_leaving_survey_responses.csv", row.names = FALSE)
+
+
+# Define paths for saving the data frames in the cache folder as .RData files
+save_path_leaving_survey_responses <- "~/Desktop/courseWorkCS8631/cache/combined_cleaned_cyber_security_leaving_survey_responses.RData"
+save_path_combined_data <- "~/Desktop/courseWorkCS8631/cache/combined_cleaned_cyber_security.RData"
+save_path_duplicates <- "~/Desktop/courseWorkCS8631/cache/duplicate_counts.RData"
+
+# Save each data frame as an .RData file in the specified cache directory
+save(duplicates_data_leaving_survey_responses, file = save_path_leaving_survey_responses)
+save(combined_data, file = save_path_combined_data)
+save(duplicate_counts, file = save_path_duplicates)
